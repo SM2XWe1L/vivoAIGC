e2d573ec0042012d98aff223e316a1d66a09a8ed
SM2XWe1L <141481426+SM2XWe1L@users.noreply.github.com>
2024-05-02 19:24:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
+import json
+import webbrowser
+
 import ai绘画.提交任务
 import ai绘画.查询
 import ai绘画.取消任务
 from 蓝心大模型 import lanxin
-from ai绘画 import 提交任务
+
 word = input()
 lanxin.sync_vivogpt(word)
 task_id = ai绘画.提交任务.submit(word)
-draw_result = ai绘画.查询.progress(task_id)
-is_finished_drawing = ai绘画.查询.progress(task_id)['result']['finished']
-while is_finished_drawing == False:##1.为何已经是true了但还是在循环2.要设置回答不出来时的绘图策略3.提问的逻辑（画的是我问的还是它回答的）4.怎么实现持续性回答问题
-    ai绘画.查询.progress(task_id)
+draw_result = None
+is_finished_drawing = False
+
+while (is_finished_drawing) == False: # 跳出该循环时，ai绘画已然完成
+    draw_result, is_finished_drawing = ai绘画.查询.progress(task_id)
+
+url_of_drawing = "".join(draw_result['result']['images_url'])      # 列表转字符串方法可不是str（）
+
+webbrowser.open(url_of_drawing)
+
+# print(draw_result['result'][])
+
+# 2.要设置回答不出来时的绘图策略
+# 3.提问的逻辑（画的是我问的还是它回答的）
+# 4.怎么实现持续性回答问题
 
 
 
